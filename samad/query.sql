7b17ad5ff675530177bd6e396be024a94246744e
mehrshad-kh <mjii@protonmail.com>
2023-01-24 23:36:56 +0330

    Removed tracking of object files

--- samad/query.sql
+++
@@ -1,7 +0,0 @@
-SELECT t.datetime AS date_and_time,
-tt.name AS transaction_type_name,
-t.amount AS amount
-FROM transactions t
-INNER JOIN transaction_types tt
-ON t.transaction_type_id = tt.rowid
-WHERE t.user_id = 4;
